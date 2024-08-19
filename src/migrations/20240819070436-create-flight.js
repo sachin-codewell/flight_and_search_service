1df99d5c49bfa4f04a25511e99d63405881e13f4
Sachin Yadav <sny2771999@gmail.com>
2024-08-19 15:52:01 +0530

    added flight modal

---
+++ src/migrations/20240819070436-create-flight.js
@@ -0,0 +1,61 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Flights', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.STRING
+      },
+      flightNumber: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      airplaneId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      departureAirportId: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      arrivalAirportId: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      departureTime: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      arrivalTime: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      price: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      boardingGate: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      totalSeat: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Flights');
+  }
+};
