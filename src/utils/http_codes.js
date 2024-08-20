c72f8f55ec020e36749798689696dd79ee5e24e6
Sachin Yadav <sny2771999@gmail.com>
2024-08-20 19:27:18 +0530

    added http codes

---
+++ src/utils/http_codes.js
@@ -0,0 +1,15 @@
+const clientErrorsResponse = Object.freeze({
+    BadRequest: 400,
+    Forbidden: 403,
+    NotFound: 404
+})
+
+const successfulResponses = Object.freeze({
+    OK: 200,
+    Created: 201,
+    Accepted: 202
+})
+
+serverErrorResponses = Object.freeze({
+    internalServerError: 500
+})
